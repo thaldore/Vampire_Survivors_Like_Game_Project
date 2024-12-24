637cbb2b8ba42c57596d4b1219f94f8cbc97e05c
fafafaruk <yasaromerfaruk1@gmail.com>
2024-12-24 13:46:48 +0300

    Revert "Silah hatasının düzeltimi"
    This reverts commit 43ac4c0b6e7b53467e2b27d61f8ff4df1abfc738.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,38 +1,37 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-/// <summary>
-/// Controls all player movement
-/// </summary>
 public class PlayerMovement : MonoBehaviour
 {
+    //Movement
     public float moveSpeed;
     Rigidbody2D rb;
-
-    [HideInInspector]
-    public Vector2 moveDir;
-
-    //To preserve states
     [HideInInspector]
     public float lastHorizontalVector;
     [HideInInspector]
     public float lastVerticalVector;
     [HideInInspector]
+    public Vector2 moveDir;
+    [HideInInspector]
     public Vector2 lastMovedVector;
+    
 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        lastMovedVector = new Vector2(1, 0f); //If we don't do this and game starts up and don't move, the projectile weapon will have no momentum
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         InputManagement();
     }
 
-    void FixedUpdate() //Always calculate physics in fixed update
+    void FixedUpdate ()
     {
         Move();
     }
@@ -42,28 +41,33 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        moveDir = new Vector2(moveX, moveY).normalized; //Use normalize as moving in diagonal generates a value > 1 so cap it to 1
+        moveDir = new Vector2(moveX, moveY).normalized;
 
         if (moveDir.x != 0)
         {
             lastHorizontalVector = moveDir.x;
-            lastMovedVector = new Vector2(lastHorizontalVector, 0f);    //Last moved X
+            lastMovedVector = new Vector2(lastHorizontalVector, 0f); // last moved x
+
         }
 
         if (moveDir.y != 0)
         {
-            lastVerticalVector = moveDir.y;
-            lastMovedVector = new Vector2(0f, lastVerticalVector);  //Last moved Y
+            lastHorizontalVector = moveDir.y;
         }
 
-        if (moveDir.x != 0 && moveDir.y != 0)
+        if (moveDir.y != 0)
         {
-            lastMovedVector = new Vector2(lastHorizontalVector, lastVerticalVector);    //While moving
+            lastHorizontalVector = moveDir.y;
+            lastMovedVector = new Vector2 (0f , lastVerticalVector); // last moved y  
         }
-    }
+        else if (moveDir.x != 0 && moveDir.y != 0)
+        {
+            //lastMovedVector = new Vector2(lastHorizontalVector, lastVerticalVector); // while moving
+        } 
 
+    }
     void Move()
     {
-        rb.linearVelocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);    //Apply velocity
+        rb.linearVelocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
     }
 }
